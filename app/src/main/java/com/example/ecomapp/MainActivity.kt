ab5ee7df6f66beb904b02b7efc093adb8e452390
Abhas Kumar <abhas1102@gmail.com>
2022-08-31 18:49:03 +0530

    added my project

---
+++ app/src/main/java/com/example/ecomapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.ecomapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.window.SplashScreen
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+        Handler().postDelayed({
+                              val intent = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        },2000)
+    }
+}
